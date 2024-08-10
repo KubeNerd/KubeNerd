514df6f8a442e1e0b5eb2c4a9f554bbaf5a35a0f
@kube_nerd <viniciuspoa2@hotmail.com>
2024-08-10 19:14:11 -0300

    Update fetchGithubData.ts

--- src/fetchGithubData.ts
+++ src/fetchGithubData.ts
@@ -1,5 +1,5 @@
 export async function fetchGitHubData(repos: Array<string>): Promise<string> {
-    const owner = "rxjsvini";
+    const owner = "KubeNerd";
   
     const list = await Promise.all(
       repos.map(async (repo) => {
